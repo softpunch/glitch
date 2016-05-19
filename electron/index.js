5d77f67d7069544145d778bc5c3a8becd4943b41
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-05-19 10:59:42 +0800

    added electron packaging

---
+++ electron/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow () {
+  let iconPath = __dirname + '/build/glitch192x192.png';
+  console.log(iconPath)
+  mainWindow = new BrowserWindow({width: 640, height: 480, icon: iconPath});
+
+  mainWindow.loadURL('file://' + __dirname + '/build/index.html');
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
